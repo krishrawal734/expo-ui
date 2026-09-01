ccff56fd77943378516161d7b4556fa7d712b041
krishrawal734 <rawalkrish734@gmail.com>
2026-09-01 17:38:57 +0530

    chore: update dependencies and improve Header component styling
    - Removed unused dependencies from package.json.
    - Added nativewind, babel-preset-expo, prettier-plugin-tailwindcss, and tailwindcss to devDependencies.
    - Updated react-native-reanimated to use caret versioning.
    - Adjusted Header component styling: increased margin and font size for better visibility.

--- src/components/subscription/Header.tsx
+++ src/components/subscription/Header.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Header() {
   return (
     <View className="h-20 justify-center bg-white px-3">
-      <Text className="mt-2.5 text-xl font-bold text-orange-600">
+      <Text className="mt-5 text-2xl font-bold text-orange-600">
         Subscription
       </Text>
     </View>
