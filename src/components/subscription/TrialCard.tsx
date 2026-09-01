ef00ef360d540fb22a49c53e81ea1e4cc500c8d0
krishrawal734 <rawalkrish734@gmail.com>
2026-09-01 10:41:57 +0530

    Refactor TrialCard component for improved code readability and consistency

--- src/components/subscription/TrialCard.tsx
+++ src/components/subscription/TrialCard.tsx
@@ -1,49 +1,35 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
-import { Colors } from "../../constants/colors";
 
 export default function TrialCard() {
   return (
     <View style={styles.container}>
-
+      {/* Text */}
       <View>
-        <Text style={styles.title}>
-          Free Trial
-        </Text>
+        <Text style={styles.title}>Free Trial</Text>
 
-        <Text style={styles.subtitle}>
-          12 days left
-        </Text>
+        <Text style={styles.subtitle}>12 days left</Text>
       </View>
 
-      <Ionicons
-        name="stopwatch"
-        size={19}
-        color="brown"
-      />
-
+      {/* Icon */}
+      <Ionicons name="stopwatch" size={19} color="brown" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 8,
-    marginTop: 15,
-
     height: 60,
+
+    marginTop: 15,
+    marginHorizontal: 8,
 
     paddingHorizontal: 12,
 
-    borderRadius: 6,
-
     backgroundColor: "#FFA066",
+
+    borderRadius: 6,
 
     flexDirection: "row",
     alignItems: "center",
@@ -54,6 +40,7 @@ const styles = StyleSheet.create({
 
   title: {
     fontSize: 15,
+
     fontWeight: "500",
 
     color: "#542400",
